c2c009f2595116e0f7012586939e2473b41f6685
j8o8 <99486802+j8o8@users.noreply.github.com>
2024-07-10 14:24:03 +0200

    Update Gen-Password.ps1

--- Gen-Password.ps1
+++ Gen-Password.ps1
@@ -1,14 +1,3 @@
-<#
-.SYNOPSIS
-Domain policy compliant password generator 
-.DESCRIPTION
-Generates a random password subject to all set domain password policies
-.EXAMPLE
-Gen-Password.ps1 -DC SV-DC01 -UserName admin -HighPriv
-.EXAMPLE
-Gen-Password.ps1 -DC SV-DC01 -NoSpecial
-#>
-
 using namespace System.Management.Automation.Host
 
 [CmdletBinding()] param(
